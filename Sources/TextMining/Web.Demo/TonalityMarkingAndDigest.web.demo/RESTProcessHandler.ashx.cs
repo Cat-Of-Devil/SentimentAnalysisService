490385c5415969ac579273d0f96a06e9047561d1
zamgi <zamgi@gmail.com>
2017-04-19 01:36:25 +0300

    ...

--- Sources/TextMining/Web.Demo/TonalityMarkingAndDigest.web.demo/RESTProcessHandler.ashx.cs
+++ Sources/TextMining/Web.Demo/TonalityMarkingAndDigest.web.demo/RESTProcessHandler.ashx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Xml;
 using System.Xml.Linq;
@@ -13,50 +15,12 @@ using Newtonsoft.Json;
 using captcha;
 using Digest;
 using OpinionMining;
-using TextMining.Core;
 using TonalityMarking;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Threading;
+using TextMining.Core;
 
 #if WITH_OM_TM
 using Lingvistics.Client;
 #endif
-
-namespace TonalityMarkingAndDigest.web.demo
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    internal static class Config
-    {
-#if WITH_OM_TM
-        public static readonly string LINGUISTICS_SERVER_URL                  = ConfigurationManager.AppSettings[ "LINGUISTICS_SERVER_URL" ];
-        public static readonly int    LINGUISTICS_SERVER_TIMEOUT_IN_SECONDS   = ConfigurationManager.AppSettings[ "LINGUISTICS_SERVER_TIMEOUT_IN_SECONDS" ].ToInt32();
-        public static readonly string LINGUISTICS_SERVER_EXE_FILE_LOCATION    = ConfigurationManager.AppSettings[ "LINGUISTICS_SERVER_EXE_FILE_LOCATION" ];
-        public static readonly bool   LINGUISTICS_SERVER_EXE_CREATE_NO_WINDOW = ConfigurationManager.AppSettings[ "LINGUISTICS_SERVER_EXE_CREATE_NO_WINDOW" ].TryToBool( false );
-
-        public static ProcessStartInfo CreateLinguisticsServerProcessStartInfo()
-        {
-            var startInfo = new ProcessStartInfo( LINGUISTICS_SERVER_EXE_FILE_LOCATION, "-console" );
-            if ( LINGUISTICS_SERVER_EXE_CREATE_NO_WINDOW )
-            {
-                startInfo.WindowStyle     = ProcessWindowStyle.Hidden;
-                startInfo.CreateNoWindow  = false;
-                startInfo.UseShellExecute = false;
-            }
-            return (startInfo);
-        }
-#else
-        public static readonly string TONALITY_MARKING_ENDPOINT_CONFIGURATION_NAME = ConfigurationManager.AppSettings[ "TONALITY_MARKING_ENDPOINT_CONFIGURATION_NAME" ];
-        public static readonly string DIGEST_ENDPOINT_CONFIGURATION_NAME           = ConfigurationManager.AppSettings[ "DIGEST_ENDPOINT_CONFIGURATION_NAME" ];
-#endif
-        public static readonly int MAX_INPUTTEXT_LENGTH                = ConfigurationManager.AppSettings[ "MAX_INPUTTEXT_LENGTH"                ].ToInt32();
-        public static readonly int SAME_IP_INTERVAL_REQUEST_IN_SECONDS = ConfigurationManager.AppSettings[ "SAME_IP_INTERVAL_REQUEST_IN_SECONDS" ].ToInt32();
-        public static readonly int SAME_IP_MAX_REQUEST_IN_INTERVAL     = ConfigurationManager.AppSettings[ "SAME_IP_MAX_REQUEST_IN_INTERVAL"     ].ToInt32();        
-        public static readonly int SAME_IP_BANNED_INTERVAL_IN_SECONDS  = ConfigurationManager.AppSettings[ "SAME_IP_BANNED_INTERVAL_IN_SECONDS"  ].ToInt32();
-    }
-}
 
 namespace TonalityMarkingAndDigest.web.demo
 {
@@ -441,34 +405,8 @@ namespace TonalityMarkingAndDigest.web.demo
     /// <summary>
     /// 
     /// </summary>
-    internal static class Extensions
+    internal static partial class Extensions
     {
-        public static bool TryToBool( this string value, bool defaultValue )
-        {
-            bool result;
-            return (bool.TryParse( value, out result ) ? result : defaultValue);
-        }
-        public static bool? TryToBool( this string value )
-        {
-            bool result;
-            return (bool.TryParse( value, out result ) ? result : ((bool?) null));
-        }
-
-        public static T ToEnum< T >( this string value ) where T : struct
-        {
-            var result = (T) Enum.Parse( typeof(T), value, true );
-            return (result);
-        }
-        public static T? TryToEnum< T >( this string value ) where T : struct
-        {
-            T t;
-            return (Enum.TryParse( value, true, out t ) ? t : ((T?) null));
-        }
-        public static int ToInt32( this string value )
-        {
-            return (int.Parse( value ));
-        }
-
         public static string GetRequestStringParam( this HttpContext context, string paramName, int maxLength )
         {
             var value = context.Request[ paramName ];
@@ -479,10 +417,6 @@ namespace TonalityMarkingAndDigest.web.demo
             return (value);
         }
 
-        public static bool IsNullOrWhiteSpace( this string value )
-        {
-            return (string.IsNullOrWhiteSpace( value ));
-        }
         public static List< string > ToTextList( this string text )
         {
             return (text.Split( new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries ).ToList());
